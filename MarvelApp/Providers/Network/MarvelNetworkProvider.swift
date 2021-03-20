3c38e30075b7686e4fd1ba13322377953b7b7404
Jon Gonzalez <jongonzalez000@gmail.com>
2021-03-20 21:16:39 +0100

    Created the first version of a function to get the characters list

--- MarvelApp/Providers/Network/MarvelNetworkProvider.swift
+++ MarvelApp/Providers/Network/MarvelNetworkProvider.swift
@@ -21,8 +21,12 @@ class MarvelNetworkProvider: MarvelProviderContract {
     
     func getCharacters() -> Promise<[Character]> {
         return Promise<[Character]> { promise in
-            AF.request(getURL(endpoint: .characters)).responseJSON { response in
-                
+            self.getInitialData(fromEndpoint: .characters).done { data in
+                let characterList = self.getCharacterList(fromData: data)
+                promise.fulfill(characterList)
+            }.catch { error in
+                #warning("TODO: improve")
+                promise.reject(error)
             }
         }
     }
@@ -34,5 +38,28 @@ class MarvelNetworkProvider: MarvelProviderContract {
             url.appendPathComponent("characters/")
             return url
         }
+    }
+    
+    private func getInitialData(fromEndpoint endpoint: MarvelURLEndpoint) -> Promise<[[String: Any]]> {
+        return Promise<[[String: Any]]> { promise in
+            AF.request(getURL(endpoint: endpoint)).responseJSON { response in
+                guard let data = try? response.result.get() as? [String: Any],
+                      let dataResultData = data["data"] as? [String: Any],
+                      let result = dataResultData["results"] as? [[String: Any]] else {
+                    return
+                }
+                promise.fulfill(result)
+            }
+        }
+    }
+    
+    private func getCharacterList(fromData data: [[String: Any]]) -> [Character] {
+        var characterList = [Character]()
+        for item in data {
+            if let character = try? Character(JSON: item) {
+                characterList.append(character)
+            }
+        }
+        return characterList
     }
 }
