381de6c69466f30dc74a4f84657515ce3303e384
Jon Gonzalez <jongonzalez000@gmail.com>
2021-03-20 20:51:53 +0100

    Created an Enum to access to apikey, hash and timestamp saved on a .plist file

--- MarvelApp/Providers/Network/NetworkConstants.swift
+++ MarvelApp/Providers/Network/NetworkConstants.swift
@@ -13,7 +13,61 @@ struct NetworkConstants {
         components.scheme = "https"
         components.host = "gateway.marvel.com"
         components.path = "/v1/public/"
+        let apiKey = URLQueryItem(name: "apikey", value: NetworkInfo.apiKey)
+        let hash = URLQueryItem(name: "hash", value: NetworkInfo.hash)
+        let ts = URLQueryItem(name: "ts", value: NetworkInfo.ts)
+        components.queryItems = [apiKey, hash, ts]
         
+        // Como la URL la estoy creando yo y en caso de que se forme de forma incorrecta o no sea la URL
+        // que necesito quiero que me falle, hago el unwrap forzado.
         return components.url!
     }
+}
+
+// El Enum NetworkInfo lo uso para leer la apiKey, hash y timestamp del archivo NetworkInfo.plist que no subo a git.
+// Lo uso en la función de arriba para añadirlos en los queryparams. No sé si es el lugar apropiado para poner esto,
+// pero como no sabía donde ponerlo, lo he definido aquí.
+enum NetworkInfo {
+    // MARK: - Keys
+    // swiftlint:disable all
+    enum Keys {
+        enum Plist {
+            static let apiKey = "API_KEY"
+            static let hash = "HASH"
+            static let ts = "TS"
+        }
+    }
+    // swiftlint:enable all
+    
+    // MARK: - Plist
+    private static let infoDictionary: [String: Any] = {
+        guard let infoPlistPath = Bundle.main.path(forResource: "NetworkInfo", ofType: "plist"),
+           let dict = NSDictionary(contentsOfFile: infoPlistPath) as? [String: Any] else {
+            fatalError("Plist file not found")
+        }
+        return dict
+    }()
+    
+    // MARK: - Plist values
+    static let apiKey: String = {
+        guard let apiKey = NetworkInfo.infoDictionary[Keys.Plist.apiKey] as? String else {
+            fatalError("API Key not set in NetworkInfo.plist")
+        }
+        return apiKey
+    }()
+    
+    static let hash: String = {
+        guard let hash = NetworkInfo.infoDictionary[Keys.Plist.hash] as? String else {
+            fatalError("Hash Key not set in NetworkInfo.plist")
+        }
+        return hash
+    }()
+    
+    static let ts: String = {
+        guard let ts = NetworkInfo.infoDictionary[Keys.Plist.ts] as? String else {
+            fatalError("Timestamp Key not set in NetworkInfo.plist")
+        }
+        return ts
+    }()
+    
 }
