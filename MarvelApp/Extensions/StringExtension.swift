bda1dafa0631a03d0a38b64b01d87636ebe9afaa
Jon Gonzalez <jongonzalez000@gmail.com>
2021-03-26 18:04:40 +0100

    Disabled swiftlint warning

--- MarvelApp/Extensions/StringExtension.swift
+++ MarvelApp/Extensions/StringExtension.swift
@@ -9,6 +9,7 @@ import Foundation
 
 extension String {
     func isValidEmail() -> Bool {
+        // swiftlint:disable:next line_length
         let regex = "^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$"
         let emailPredicate = NSPredicate(format: "SELF MATCHES %@", regex)
         return emailPredicate.evaluate(with: self)
