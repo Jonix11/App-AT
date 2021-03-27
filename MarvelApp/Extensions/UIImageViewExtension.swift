f86daa9daa4dbabf3429296d8f45afc70728f2b5
Jon Gonzalez <jongonzalez000@gmail.com>
2021-03-27 12:55:57 +0100

    Created an UIImageView extension to make image rounded

---
+++ MarvelApp/Extensions/UIImageViewExtension.swift
@@ -0,0 +1,15 @@
+//
+//  UIImageViewExtension.swift
+//  MarvelApp
+//
+//  Created by Jon Gonzalez on 27/3/21.
+//
+
+import UIKit
+
+extension UIImageView {
+    func roundedImage() {
+        self.layer.cornerRadius = self.frame.height / 2.2
+        self.clipsToBounds = true
+    }
+}
